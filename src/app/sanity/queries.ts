9c147cf2def788546c463fd022cf903f1a1dfc19
Edwin Jordan Herbas <ejordanherbas@gmail.com>
2025-11-11 17:24:02 +0100

    Update queries.ts

--- src/app/sanity/queries.ts
+++ src/app/sanity/queries.ts
@@ -1,10 +1,10 @@
+// src/sanity/queries.ts
 import {groq} from 'next-sanity'
 
-export const heroQuery = groq`*[_type == "hero"][0]{
-  title,
-  subtitle,
-  ctaText,
-  ctaHref,
-  bgColor,
+export const activePromotionsQuery = groq`
+*[_type=="promotion" && (active==true || (defined(startsAt) && startsAt<=now() && (!defined(endsAt) || endsAt>=now())))]
+| order(_createdAt desc)[0...6]{
+  _id, title, description, price,
   "imageUrl": image.asset->url
-}`
+}
+`
