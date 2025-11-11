f6a25e2468104e4353644fbbe4665dccc5cfa2c0
Edwin Jordan Herbas <ejordanherbas@gmail.com>
2025-11-11 17:23:47 +0100

    Update fetch.ts

--- src/app/sanity/fetch.ts
+++ src/app/sanity/fetch.ts
@@ -1,6 +1,10 @@
-import { client } from '@/lib/sanity.client'
-import { heroQuery } from './queries'
+// src/sanity/fetch.ts
+import {client} from '@/lib/sanity.client'
+import {heroQuery, activePromotionsQuery} from './queries'
 
 export async function getHero() {
-  return client.fetch(heroQuery)
+  return client.fetch(heroQuery, {}, { next: { tags: ['hero'] } })
+}
+export async function getActivePromotions() {
+  return client.fetch(activePromotionsQuery, {}, { next: { tags: ['promotions'] } })
 }
